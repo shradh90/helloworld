ffc3c990c25cb30c2ad7c43b88a72c0f33dfda7d
shradh90 <shradhad1996@gmail.com>
2021-06-09 10:25:43 +0530

    initial commit

--- hello.java
+++ hello.java
@@ -5,6 +5,8 @@ package hello;
 public class helloworld {
 	public static void main(String[] args) {
 				System.out.println("hello world");
+				System.out.println("changes commited in local branch");
+				System.out.println("changes done in remote master");
 			}
 }
 	
